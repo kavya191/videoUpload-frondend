f6b0753ddf5c81e5f5c629df84d0d5f028ce0739
kavya191 <kavyabose191@gmail.com>
2023-10-11 13:31:55 +0530

    video committed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Upload } from 'react-feather'
 function Footer() {
   return (
-    <div className='bg-primary p-1 text-center  mt-5' data-bs-theme="dark" style={{width:'100vw'}}>
+    <div className='bg-primary p-1 text-center mt-5' data-bs-theme="dark" style={{width:'100vw'}}>
       <p className='text-white  fw-normal mt-2'>All Rights Reserved@<span><Upload></Upload></span>video uploader</p>
 
     </div>
